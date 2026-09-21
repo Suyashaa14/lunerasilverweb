457c0afab5785c995dcb4d72ef3cea1d7b8772ba
sabin sunar <sabinsunar577@gmail.com>
2026-09-22 00:50:03 +0545

    Add .htaccess for URL rewriting and update deploy script for dynamic source directory

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,14 @@
 
 set -e
 
-SOURCE_DIR="/home/luneraon/lunerasilverweb/dist"
+SOURCE_DIR="$(pwd)/dist"
 TARGET_DIR="/home/luneraon/public_html/lunerasilverweb"
 
 echo "🚀 Deploying frontend..."
 
-echo "🧹 Removing old build files..."
-
 find "$TARGET_DIR" -mindepth 1 \
     ! -name ".htaccess" \
     -exec rm -rf {} +
-
-echo "📦 Copying new build..."
 
 cp -R "$SOURCE_DIR/." "$TARGET_DIR/"
 
